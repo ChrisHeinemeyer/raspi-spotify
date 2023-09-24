ac5efcba64ba7ad89f43cfc018df17e60c995dc2
Chris Heinemeyer <chris.heinemeyer@ouster.io>
2023-09-24 11:40:44 -0700

    logging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import platform
 from dataclasses import asdict
 from io import BytesIO
@@ -15,12 +16,14 @@ from track import Album, Track
 
 SECRETS_FILE = "secrets.yaml"  # pragma: allowlist secret
 CONFIG_FILE = "config.yaml"
+LOG_FILE = "log.log"
 ALBUM_INFO_FILE = "album_info.json"
 HEIGHT_PX = 448
 WIDTH_PX = 600
 
 
 def get_most_played_album() -> Album:
+    logger = logging.getLogger(__name__)
     with open(SECRETS_FILE) as f:
         secrets = yaml.safe_load(f)
 
@@ -45,13 +48,17 @@ def get_most_played_album() -> Album:
     for uri in set(uris):
         count = uris.count(uri)
         counts.append((albums[uri], count))
-
     counts = sorted(counts, key=lambda x: x[1], reverse=True)
+    logger.debug([f"{a.artists[0].name} -  {a.name}: {c}" for a, c in counts])
+    most_played_album = counts[0][0]
+    logger.debug(f"Most played album: {most_played_album.name}")
     return counts[0][0]
 
 
 def get_image_from_album(album: Album) -> Image:
     #   TODO find best sized image
+    logger = logging.getLogger(__name__)
+    logger.debug(f"Using image {asdict(album.images[0])}")
     url = album.images[0].url
     raw_img = requests.get(url)
     return Image.open(BytesIO(raw_img.content))
@@ -59,7 +66,6 @@ def get_image_from_album(album: Album) -> Image:
 
 def resize_img(im: Image, height_px: int, width_px: int) -> Image:
     #  TODO remove assumption height is smaller than width
-
     im = im.resize((height_px, height_px))
     bar_width_px = (width_px - height_px) // 2
     base_img = Image.new("RGB", (width_px, height_px), (0, 0, 0))
@@ -67,23 +73,30 @@ def resize_img(im: Image, height_px: int, width_px: int) -> Image:
     return base_img
 
 
-if __name__ == "__main__":
-    with open(CONFIG_FILE) as f:
-        config = yaml.safe_load(f)
+def main(config):
+    logger = logging.getLogger(__name__)
 
+    logger.debug("Starting")
+    logger.debug(f"Using config {config}")
     try:
         with open(ALBUM_INFO_FILE, "r") as f:
             info = json.loads(f.read())
+            previous_album = from_dict(Album, info)
+        logger.debug(
+            f"Previous record {previous_album.artists[0].name} - {previous_album.name} found"
+        )
     except FileNotFoundError:
+        logger.info(f"No previous record found")
         info = None
 
     album = get_most_played_album()
     need_update = True
     if info and info["uri"] == album.uri:
-        print("don't do it!")
+        logger.debug("Previous album matches current album")
         need_update = False
 
     if need_update or config["force_reload"]:
+        logger.debug("Proceeding to get image")
         img = get_image_from_album(album)
         img = resize_img(img, HEIGHT_PX, WIDTH_PX)
         img.save("img.png")
@@ -91,9 +104,33 @@ if __name__ == "__main__":
             f.write(json.dumps(asdict(album)))
 
         if platform.system() == "Linux":
-            print("setting display")
+            logger.debug("Setting display")
             from inky.auto import auto
 
             display = auto()
             display.set_image(img)
             display.show()
+            logger.debug("Done setting display")
+        else:
+            logger.debug(f"Can't set display on platform {platform.system()}")
+    else:
+        logger.debug("Not going to get or set image")
+
+    logger.debug("Finished")
+
+
+if __name__ == "__main__":
+    with open(CONFIG_FILE) as f:
+        config = yaml.safe_load(f)
+    logging.basicConfig(
+        filename=LOG_FILE,
+        filemode="a",
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s: %(message)s",
+    )
+    logging.getLogger().addHandler(logging.StreamHandler())
+
+    logging.getLogger("spotipy").setLevel(logging.INFO)
+    logging.getLogger("requests").setLevel(logging.INFO)
+    logging.getLogger("urllib3").setLevel(logging.INFO)
+    main(config)
--- track.py
+++ track.py
@@ -26,6 +26,7 @@ class Album:
     images: List[Image]
     name: str
     uri: str
+    artists: List[Artist]
 
 
 @dataclass
