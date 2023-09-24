0e7f677f59d55b82e2f7b4ba4451b200468cd5ca
Chris Heinemeyer <chris.heinemeyer@ouster.io>
2023-09-24 09:06:45 -0700

    resize for display

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from io import BytesIO
-from pprint import pprint
 from typing import Dict, List, Tuple
 
 import requests
@@ -12,6 +11,8 @@ from spotipy.oauth2 import SpotifyOAuth
 from track import Album, Track
 
 SECRETS_FILE = "secrets.yaml"  # pragma: allowlist secret
+HEIGHT_PX = 448
+WIDTH_PX = 600
 
 
 def get_most_played_album() -> Album:
@@ -29,7 +30,6 @@ def get_most_played_album() -> Album:
     )
     tracks = []
     results = sp.current_user_recently_played()
-    pprint(results, indent=2)
     for idx, item in enumerate(results["items"]):
         track = from_dict(Track, item["track"])
         tracks.append(track)
@@ -45,15 +45,24 @@ def get_most_played_album() -> Album:
     return counts[0][0]
 
 
-def get_image_from_album(album: Album, px: int) -> Image:
+def get_image_from_album(album: Album) -> Image:
     #   TODO find best sized image
     url = album.images[0].url
     raw_img = requests.get(url)
-    img = Image.open(BytesIO(raw_img.content))
-    return img.resize((px, px))
+    return Image.open(BytesIO(raw_img.content))
+
+
+def resize_img(im: Image, height_px: int, width_px: int) -> Image:
+    #  TODO remove assumption height is smaller than width
+
+    im = im.resize((height_px, height_px))
+    bar_width_px = (width_px - height_px) // 2
+    base_img = Image.new("RGB", (width_px, height_px), (0, 0, 0))
+    base_img.paste(im, (bar_width_px, 0))
+    return base_img
 
 
 if __name__ == "__main__":
     album = get_most_played_album()
-    img = get_image_from_album(album, 480)
-    print(img)
+    img = get_image_from_album(album)
+    img = resize_img(img, HEIGHT_PX, WIDTH_PX)
