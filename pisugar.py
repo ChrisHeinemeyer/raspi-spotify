e8c6b20a24efd9b8de7dcfe5a71a4b0969b31589
Chris Heinemeyer <chris.heinemeyer@ouster.io>
2023-09-29 20:15:16 -0700

    alarm stuff

--- pisugar.py
+++ pisugar.py
@@ -9,8 +9,16 @@ PORT = 8423
 
 
 class PisugarCommands(Enum):
-    GET_BATTERY = "get battery"
-    GET_RTC_TIME = "get rtc_time"
+    GET_BATTERY = "get battery"  # battery level %
+    GET_RTC_TIME = "get rtc_time"  # rtc clock
+
+    RTC_WEB = "rtc_web"  # sync time web => rtc & pi
+    RTC_PI2RTC = "rtc_pi2rtc"  # sync time pi => rtc
+    RTC_RTC2PI = "rtc_rtc2pi"  # sync time rtc => pi
+
+    RTC_ALARM_SET = "rtc_alarm_set"  # set rtc wakeup alarm
+    GET_RTC_ALARM_TIME = "get rtc_alarm_time"
+    GET_RTC_ALARM_ENABLED = "get rtc_alarm_enabled"
 
 
 class Pisugar:
@@ -33,8 +41,10 @@ class Pisugar:
             ]
         )
 
-    def get_command(self, cmd: PisugarCommands, *args):
-        send_str = f"{cmd.value}"
+    def send_command(self, cmd: PisugarCommands, *args):
+        args = [cmd.value] + list(args)
+        send_str = " ".join([str(a) for a in args])
+
         self.logger.debug(f"Sending {send_str.encode()}")
         self.sock.send(send_str.encode())
         res = self.sock.recv(1024)
@@ -50,5 +60,8 @@ if __name__ == "__main__":
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.DEBUG)
     ps = Pisugar()
-    for command in PisugarCommands:
-        res = ps.get_command(command)
+
+    ps.send_command(PisugarCommands.RTC_WEB)
+    ps.send_command(PisugarCommands.RTC_ALARM_SET, "2023-09-29T20:17:30.000-07:00")
+    ps.send_command(PisugarCommands.GET_RTC_ALARM_TIME)
+    ps.send_command(PisugarCommands.GET_RTC_ALARM_ENABLED)
